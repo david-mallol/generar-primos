42771eb12b1adf6bec3367eb3d6e392e1f6dcf8e
David Mallol <david@diemtic.es>
2023-03-05 23:46:57 +0100

    Programa documentado

--- Criba.java
+++ Criba.java
@@ -3,11 +3,21 @@ import java.util.Scanner;
 
 public class Criba {
 
+    /**
+     * Inicializa un array para hacer la criba de Erastótenes para calcular primos
+     * Pone a true todo el array salvo las dos primeras posiciones.
+     * @param esPrimo Array que indica si su índice es primo o no
+     */
     public static void inicializacion(boolean [] esPrimo) {
         Arrays.fill(esPrimo, true);
         esPrimo[0] = esPrimo[1] = false;
     }
 
+    /**
+     * Función que cuenta los números primos en un array (cada true es un primo)
+     * @param esPrimo Array que indica si su índice es primo o no
+     * @return devuelve el número de primos
+     */
     public static int cuentaPrimos(boolean [] esPrimo) {
         int cuenta = 0;
         for (boolean b : esPrimo) {
@@ -17,6 +27,11 @@ public class Criba {
         return cuenta;
     }
 
+    /**
+     * Partiendo de un array de booleanos crea un array de primos
+     * @param primos Array con los números primos
+     * @param esPrimo Array que indica si su índice es primo o no
+     */
     public static void rellenaPrimos(int []primos, boolean[] esPrimo) {
         for (int i = 0, j = 0; i < esPrimo.length; i++) {
             if (esPrimo[i])
@@ -24,6 +39,10 @@ public class Criba {
         }
     }
 
+    /**
+     * Implementación de la criba de Erastótenes
+     * @param esPrimo Array que indica si su índice es primo o no
+     */
     public static void criba(boolean []esPrimo) {
         for (int i = 2; i < Math.sqrt(esPrimo.length) + 1; i++) {
             if (esPrimo[i]) {
@@ -34,6 +53,11 @@ public class Criba {
 
     }
 
+    /**
+     * Genera primos hasta el valor max
+     * @param max Valor máximo de los números primos a buscar
+     * @return devuelve un array con los números primos
+     */
     public static int[] generarPrimos(int max) {
         int[] primos;
         if (max >= 2) {
@@ -50,6 +74,11 @@ public class Criba {
         return primos;
     }
 
+    /**
+     * Imprime por consola un vector
+     * @param vector    Vector a imprimir de enteros
+     * @param inicial   Indica si es el vector inicial o el vector de primos (se imprimen diferente)
+     */
     public static void imprimirVector(int []vector, boolean inicial) {
         for (int i = 0; i < vector.length; i++) {
             if (i % 10 == 0) System.out.println();
@@ -62,6 +91,10 @@ public class Criba {
 
     }
 
+    /**
+     * Programa principal para el cálculo de primos usando la criba de Erastótenes
+     * @param args  Argumentos por defecto en aplicación de consola
+     */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
